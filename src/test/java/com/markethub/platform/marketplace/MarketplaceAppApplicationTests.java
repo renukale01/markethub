fb8c7c6a437c4e06147d7c5ef55a96b1e45eba96
Renuka Kale <renuka.22010895@viit.ac.in>
2025-07-19 17:30:21 +0530

    added packages

---
+++ src/test/java/com/markethub/platform/marketplace/MarketplaceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markethub.platform.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
