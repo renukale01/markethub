cef95fae72c27d483a67a05ba35aab677cafb406
vaishnavitodkar <vaishnavi.22010803@viit.ac.in>
2025-07-29 21:00:20 +0530

    shop apis

--- src/main/java/com/markethub/platform/marketplace/repository/ShopRepository.java
+++ src/main/java/com/markethub/platform/marketplace/repository/ShopRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.markethub.platform.marketplace.entity.Shop;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
 }
